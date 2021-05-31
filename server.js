af0df9636c8d9353f1227e3c24f50d29be95f067
dennis <dennis@denniss-MacBook-Pro.local>
2021-05-31 14:56:44 +0300

    package.json start command changed to server file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 80
+
+app.get('/', (req, res) => {
+  res.end('<h1>Home page server file!</h1>')
+})
+
+app.get('/about', (req, res) => {
+  res.end('<h1>About page!</h1>')
+})
+
+app.get('/support', (req, res) => {
+  res.end('<h1>Support page!</h1>')
+})
+
+app.listen(PORT, () => [console.log('Server has been started...')])
