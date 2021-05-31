25563c3a63b50ca0354f0cac942a7ded2a1f672f
dennis <dennis@denniss-MacBook-Pro.local>
2021-05-31 16:30:57 +0300

    config vars testing

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ const app = express()
 const PORT = process.env.PORT || 80
 
 app.get('/', (req, res) => {
-    res.end('<h1>Home page!</h1>')
-
+  res.end('<h1>Home page!</h1>')
 })
 
 app.get('/about', (req, res) => {
-    res.end('<h1>About page!</h1>')
+  res.end('<h1>About page!</h1>')
 })
 
 app.get('/support', (req, res) => {
-    res.end('<h1>Support page!</h1>')
+  res.end('<h1>Support page!</h1>')
 })
 
-app.listen(PORT, () => [
-    console.log('Server has been started...')
-])
+app.listen(PORT, () => [console.log('Server has been started...')])
